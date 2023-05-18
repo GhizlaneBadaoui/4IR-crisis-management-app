2b3e5a0659de06cf46eaef22ab94b5bb2edf880d
GhizlaneBadaoui <ghizlane.badaoui@insa-lyon.fr>
2023-05-18 21:17:27 +0200

    add alert tables and navigation menu

--- src/main/resources/static/scripts1.js
+++ src/main/resources/static/scripts1.js
@@ -1,5 +1,5 @@
 var stompClient = null;
-var notificationCount = 0;
+var responseCount = 0;
 
 $(document).ready(function() {
     console.log("app page is ready");
@@ -9,13 +9,11 @@ $(document).ready(function() {
         sendMessage();
     });
 
-//    $("#send-private").click(function() {
-//        sendPrivateMessage();
-//    });
-//
-//    $("#notifications").click(function() {
-//        resetNotificationCount();
-//    });
+    $("#cancel").click(function() {
+       $('#alertType :selected').text() = "";
+       $('#content :selected').text() = "";
+       $('#location :selected').text() = "";
+    });
 });
 
 function connect() {
@@ -23,7 +21,7 @@ function connect() {
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
         console.log('Connected: ' + frame);
-        //updateNotificationDisplay();
+
         stompClient.subscribe('/topic/messages', function (message) {
             console.log("Rock and roll baby");
             console.log("message received--> "+message.body);
@@ -36,19 +34,23 @@ function connect() {
 //
 //        stompClient.subscribe('/topic/global-notifications', function (message) {
 //            console.log("Received it from global-notifications:"+message.body);
-//            notificationCount = notificationCount + 1;
 //            updateNotificationDisplay();
 //        });
 //
 //        stompClient.subscribe('/user/topic/private-notifications', function (message) {
-//            notificationCount = notificationCount + 1;
 //            updateNotificationDisplay();
 //        });
     });
 }
 
 function showMessage(message) {
-    $("#messages").append("<tr><td>" + message + "</td></tr>");
+    $("#messages").append("<tr>"
+                           + "<td>" + message.alertType + "</td>"
+                           + "<td>" + (new Date).getDate()+'/'+(new Date).getMonth()+'/'+(new Date).getFullYear()+ ' ' +(new Date).getHours() + ':' +(new Date).getMinutes()+ "</td>"
+                           + "<td>" + message.location + "</td>"
+                           + "<td>" + responseCount + "</td>"
+                           + "</tr>");
+
     if ("Notification" in window) {
       console.log("Notification pris en compte");
       if(Notification.permission!=="denied"){
@@ -61,7 +63,7 @@ function showMessage(message) {
         });
       }
       else if(Notification.permission==="granted"){
-        afficherNotification("Alerte !! ", {body:"Type: "+message.alertType+ "\nDétail: " +message.content+ " \nLieu: " + message.location});
+        afficherNotification("🚨 Alerte ! ", {body:"Type: "+message.alertType+ "\nDétail: " +message.content+ " \nLieu: " + message.location});
       }
 
     } else {
@@ -71,36 +73,16 @@ function showMessage(message) {
 
 function sendMessage() {
     console.log("sending message");
-    stompClient.send("/ws/message", {}, JSON.stringify({'messageContent': $("#message").val(),
-                                                         'location': $('#lieu :selected').text(),
-                                                         'alertType': $('#title :selected').text()}));
+    stompClient.send("/ws/message", {}, JSON.stringify({'messageContent': $("#content").val(),
+                                                         'location': $('#location :selected').text(),
+                                                         'alertType': $('#alertType :selected').text()}));
 }
 
-function sendPrivateMessage() {
-    console.log("sending private message");
-    stompClient.send("/ws/private-message", {}, JSON.stringify({'messageContent': $("#private-message").val()}));
-}
-
-function updateNotificationDisplay() {
-    if (notificationCount == 0) {
-        $('#notifications').hide();
-    } else {
-        $('#notifications').show();
-        $('#notifications').text(notificationCount);
-    }
-}
-
-function resetNotificationCount() {
-    notificationCount = 0;
-    updateNotificationDisplay();
-}
+//function sendPrivateMessage() {
+//    console.log("sending private message");
+//    stompClient.send("/ws/private-message", {}, JSON.stringify({'messageContent': $("#private-message").val()}));
+//}
 
 function afficherNotification(titre,options){
     var notification = new Notification(titre,options);
-}
-
-function onClickYes() {
-}
-
-function onClickNo() {
 }
